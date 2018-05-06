c2a751dec0389570433a724b920c2fa0586a50b2
Andrew S <sekachov@gmail>
2018-05-06 14:27:05 +0300

    решены 5 и 6 задачи

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@
  Посмотрите как работает forEach и повторите это поведение для массива, который будет передан в параметре array
  */
 function forEach(array, fn) {
+
     for (let i = 0; i < array.length; i++) {
         
         fn(array[i], i, array);
@@ -20,12 +21,14 @@ function forEach(array, fn) {
  Посмотрите как работает map и повторите это поведение для массива, который будет передан в параметре array
  */
 function map(array, fn) {
+
     let newArr = [];
 
     for (let i = 0; i < array.length; i++) {
         
         newArr.push(fn(array[i], i, array));
     }
+
     return newArr;
 }
 
@@ -37,6 +40,7 @@ function map(array, fn) {
 
  */
 function reduce(array, fn, initial) {
+    
     let previousValue = initial || array[0];
     let i = initial ? 0 : 1;
    
@@ -57,7 +61,9 @@ function reduce(array, fn, initial) {
    upperProps({ name: 'Сергей', lastName: 'Петров' }) вернет ['NAME', 'LASTNAME']
  */
 function upperProps(obj) {
+
     let result = [];
+
     for (const prop in obj) {
         result.push(prop.toUpperCase());
     }
@@ -71,7 +77,20 @@ function upperProps(obj) {
  Напишите аналог встроенного метода slice для работы с массивами
  Посмотрите как работает slice и повторите это поведение для массива, который будет передан в параметре array
  */
-function slice(array, from, to) {
+function slice(array, from = 0, to = array.length) {
+
+    let result = [];
+    let start = (from < 0) ? from + array.length : from;
+    let end = (to < 0) ? to + array.length : to;
+
+    for (let i = 0; i < array.length; i++) {
+        if (i >= start && i < end) {
+
+            result.push(array[i]);
+        }   
+    }
+
+    return result;
 }
 
 /*
@@ -81,6 +100,13 @@ function slice(array, from, to) {
  Proxy должен перехватывать все попытки записи значений свойств и возводить это значение в квадрат
  */
 function createProxy(obj) {
+
+    return new Proxy (obj, {
+        set(obj, prop, value) {
+
+            return obj[prop] = Math.pow(value, 2);
+        }
+    })
 }
 
 export {
